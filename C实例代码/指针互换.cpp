8a0b7201ae2b4a0f57aa10c04b18943a69f618bc
yzcyayaya <49707573+yzcyayaya@users.noreply.github.com>
2020-11-19 22:20:48 +0800

    使用指针互换两个数的值

---
+++ C实例代码/指针互换.cpp
@@ -0,0 +1,18 @@
+# include <stdio.h>
+void huhuan(int * p, int * q);//函数申明 
+
+int main(void)
+{
+	int a = 3;
+	int b = 5;
+	huhuan(&a, &b);//需要加取地址，因为指针需要它的地址// 
+	printf("a = %d b = %d\n", a, b);
+	return 0;
+}
+void huhuan(int * p, int * q)//int*类型， 存放的是实参的地址；// 
+{
+	int t;
+	t = *p;// 因为p指向了a，那么*p就是等于a； 
+	*p = *q;
+	*q = t;
+}
